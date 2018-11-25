4cab05c94ef21d700d55c943c4a0e9da015b02ff
Alex Power <powera17@hotmail.com>
2018-11-25 18:38:32 -0500

    Fix web controller

--- data-layer/src/main/java/com/beefy/dudes/decisiontracking/rest/HelloController.java
+++ data-layer/src/main/java/com/beefy/dudes/decisiontracking/rest/HelloController.java
@@ -1,13 +1,13 @@
 package com.beefy.dudes.decisiontracking.rest;
 
 import java.util.concurrent.atomic.AtomicLong;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import com.beefy.dudes.decisiontracking.model.Greeting;
 
-@Controller
+@RestController
 public class HelloController {
   private static final String template = "Hello, %s!";
   private final AtomicLong counter = new AtomicLong();
