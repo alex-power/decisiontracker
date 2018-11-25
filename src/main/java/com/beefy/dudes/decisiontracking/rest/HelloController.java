c2858436b2e80115fb90693d51a5e7b14be0958b
Alex Power <powera17@hotmail.com>
2018-11-25 16:01:58 -0500

    pom changes

--- src/main/java/com/beefy/dudes/decisiontracking/rest/HelloController.java
+++ src/main/java/com/beefy/dudes/decisiontracking/rest/HelloController.java
@@ -3,8 +3,8 @@ package com.beefy.dudes.decisiontracking.rest;
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import com.beefy.dudes.decisiontracking.model.Greeting;
 
 @Controller
@@ -12,8 +12,12 @@ public class HelloController {
   private static final String template = "Hello, %s!";
   private final AtomicLong counter = new AtomicLong();
 
+  @RequestMapping("/")
+  public String index() {
+    return "Greetings from Spring Boot!";
+  }
+
   @GetMapping("/hello-world")
-  @ResponseBody
   public Greeting sayHello(
       @RequestParam(name = "name", required = false, defaultValue = "Stranger") String name) {
     return new Greeting(counter.incrementAndGet(), String.format(template, name));
