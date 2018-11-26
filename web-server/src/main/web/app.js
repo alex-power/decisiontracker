bb3f344f2c3556501635aab61644c323cc4b07c6
Clay Plumridge <plumridgec@gmail.com>
2018-11-25 19:36:06 -0500

    Quick rename Adding static resources Building JS and CSS from TS and SCSS

---
+++ web-server/src/main/web/app.js
@@ -0,0 +1,15 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>LET US ACTUALLY COMMENCE</div>
+        );
+    }
+}
+
+ReactDOM.render(
+	<App />,
+	document.getElementById('react')
+)
